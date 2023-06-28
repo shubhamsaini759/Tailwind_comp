7c8f7cd277048cea7d27940ac8734309f27c893b
shubham saini <shubhamsaini759@gmail.com>
2023-06-28 20:55:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import ArticleOne from './components/Articles/ArticleOne';
+import ArticleTwo from './components/Articles/ArticleTwo';
+import DarkMode from './components/DarkMode';
+import Header from './components/Header';
+import Login from './components/Login';
+import RatingOne from './components/Rating/RatingOne';
+import RatingTwo from './components/Rating/RatingTwo';
+import ScrollAble from './components/Scrollable';
+import SideBar from './components/SideBar';
+import Skeleton from './components/Skeleton';
+import TeamOne from './components/Teams/TeamOne';
+import TeamThree from './components/Teams/TeamThree';
+import TeamTwo from './components/Teams/TeamTwo';
+import Toast from './components/Toast';
+import Cards from './components/cards';
+import CardFive from './components/cards/CardFive';
+import CardFour from './components/cards/CardFour';
+import CardSeven from './components/cards/CardSeven';
+import CardSix from './components/cards/CardSix';
+import CardThree from './components/cards/CardThree';
+import CardTwo from './components/cards/CardTwo';
+import Footer from './components/footer';
+import Invoices from './components/invoices';
+
+function App() {
+  return (
+    <div className="w-auto  ">
+      {/* <DarkMode /> */}
+      {/* <ScrollAble /> */}
+      {/* <Header /> */}
+      {/* <Cards />
+      <CardTwo /> */}
+     {/* <Login /> */}
+     {/* <CardThree />
+      <CardFour /> 
+      <Footer /> */}
+      {/* 
+      <Skeleton />
+      <Toast />
+      <Invoices /> */}
+      {/* <SideBar /> */}
+      {/* <CardFive /> */}
+      {/* <CardSix /> */}
+      {/* <TeamOne /> */}
+      {/* <TeamTwo /> */}
+      {/* <TeamThree /> */}
+      {/* <CardSeven /> */}
+      {/* <RatingOne /> */}
+      {/* <RatingTwo /> */}
+
+      {/* <ArticleOne /> */}
+      <ArticleTwo />
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
